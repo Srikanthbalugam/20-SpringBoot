8a6db027ab443bd69c7a743ea8e065b672ca4c9d
Srikanth Balugam <sb19407@corp.ojas-it.com>
2020-09-28 11:01:35 +0530

    feature-300 changes done

--- src/main/java/com/example/beans/Car.java
+++ src/main/java/com/example/beans/Car.java
@@ -1,12 +1,14 @@
 package com.example.beans;
 
 public class Car {
-	
+	private static final int=200;
 	public void start()
 	{
 		//this is the h-122 task
 		
 		int var=122;
 	}
+	//feature-300 changes
+	public void doChange(){}
 	
 }
